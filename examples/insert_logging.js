f5265d01a85855a3e04539dcea81927c65819a81
Robert Masen <r.f.masen@gmail.com>
2019-09-04 16:29:05 -0500

    more features to insert_logging example

--- examples/insert_logging.js
+++ examples/insert_logging.js
@@ -1,4 +1,7 @@
 function Thing() {
+    if (!new.target) {
+        return new Thing();
+    }
     this.add = function(one, two) {
         return one + two;
     }
@@ -13,16 +16,25 @@ class Stuff {
 
     }
 
-    add(one, two) {
-        return one + two;
+    add(one, two){
+        class InnerStuff {
+            constructor() {
+    
+            }
+            add(three, four) {
+                return three + four;
+            }
+        }
+        return new InnerStuff().add(one, two);
     }
 }
 
 let x = (a) => a + 1;
-
+let z = ({x, y}) => x + y;
 (function() {
     let t = Thing();
-    let a = add(1, 2);
+    t.add(1, 2)
+    let a = add(3, 4);
     let s = new Stuff();
-    s.add(3, 4);
+    s.add(5, 6);
 })();
