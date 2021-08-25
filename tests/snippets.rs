fed134e3771530db5f803b00ba8e4e9c596b6a2b
rfm <r@robertmasen.pizza>
2021-08-24 23:19:22 -0500

    use ast test

--- tests/snippets.rs
+++ tests/snippets.rs
@@ -4,39 +4,29 @@ mod common;
 fn nested_conditional() {
     pretty_env_logger::try_init().ok();
     let js = "let a = (a ? b : c) ? d : e;";
-    let (a, b) = common::double_round_trip(js, false);
-    assert_eq!(a.trim(), js);
-    assert_eq!(b.unwrap().trim(), js);
+    common::round_trip_validate(js, false, "nested_conditional").unwrap();
 }
 #[test]
 fn new_call_member() {
     pretty_env_logger::try_init().ok();
     let js = "new (c(d).e)();";
-    let (a, b) = common::double_round_trip(js, false);
-    assert_eq!(a.trim(), js);
-    assert_eq!(b.unwrap().trim(), js);
+    common::round_trip_validate(js, false, "new_call_member").unwrap();
 }
 #[test]
 fn call_conditional() {
     pretty_env_logger::try_init().ok();
     let js = "(a ? b : c)();";
-    let (a, b) = common::double_round_trip(js, false);
-    assert_eq!(a.trim(), js);
-    assert_eq!(b.unwrap().trim(), js);
+    common::round_trip_validate(js, false, "call_conditional").unwrap();
 }
 #[test]
 fn call_logical() {
     pretty_env_logger::try_init().ok();
     let js = "(a || c)();";
-    let (a, b) = common::double_round_trip(js, false);
-    assert_eq!(a.trim(), js);
-    assert_eq!(b.unwrap().trim(), js);
+    common::round_trip_validate(js, false, "call_logical").unwrap();
 }
 #[test]
 fn assign_prop_in_arg() {
     pretty_env_logger::try_init().ok();
     let js = "let _ = (a, {b} = {b: 3}) => (a + b);";
-    let (a, b) = common::double_round_trip(js, false);
-    assert_eq!(a.trim(), js);
-    assert_eq!(&a, b.as_ref().unwrap());
+    common::round_trip_validate(js, false, "assign_prop_in_arg").unwrap();
 }
